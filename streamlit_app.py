5537019243854d3dc867a12aad16c26facef1d06
oshriva <54990510+oshriva@users.noreply.github.com>
2022-07-13 17:56:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,7 @@ try:
     else:
         back_from_function=get_fruity_vice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
+        
 except URLError as e:
   streamlit.error()
   
